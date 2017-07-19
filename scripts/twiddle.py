be3b142176cdba312fb3d266b92b6a5f3888b9a3
Fabian Meyer <user8324@posteo.net>
2017-07-19 10:56:58 +0200

    Update output format of twiddle generator

--- scripts/twiddle.py
+++ scripts/twiddle.py
@@ -25,14 +25,20 @@ def parse_args():
 def calc_twiddle(n):
     result = []
 
-    for i in range(n):
-        tf = (round(math.cos(2 * math.pi * i / n)),
+    for i in range(int(n / 2)):
+        tf = (round(math.cos(2 * math.pi * i / n), 4),
               round(math.sin(2 * math.pi * i / n), 4))
         result.append(tf)
 
     return result
 
 
+def twiddle_to_str(twiddle):
+    tf_strs = [str(tf) for tf in twiddle]
+    return '(\n    {}\n)'.format(',\n    '.join(tf_strs))
+
+
 if __name__ == '__main__':
     cfg = parse_args()
-    print(calc_twiddle(cfg.n))
+    twiddle = calc_twiddle(cfg.n)
+    print(twiddle_to_str(twiddle))
